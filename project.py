f4265d3b3ae0b1c49e8cc4aa4dfb8f2cc8a28333
{Tanishkraj2005} <{rajtanishk475@gmail.com}>
2025-04-12 01:23:55 +0530

    Setup codebase - Libraries and imports

--- project.py
+++ project.py
@@ -1,3 +1,17 @@
-# Project Setup - Good Initial Commit
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from scipy.stats import pearsonr
 
-print('Good, Initial Commit')
+# Base CSV load and preprocessing
+fp = "D:/Python Project/Water_Consumption_And_Cost__2013_-_Feb_2025_.csv"
+df = pd.read_csv(fp)
+
+# Fix Revenue Month parsing
+df['Revenue Month'] = pd.to_datetime(df['Revenue Month'], errors='coerce')
+df = df.dropna(subset=['Revenue Month', 'Consumption (HCF)', 'Current Charges'])
+df['Year'] = df['Revenue Month'].dt.year
+df['Month'] = df['Revenue Month'].dt.month
